6b3fa455458c3cb907bb0443c231094360871e07
li361028458 <361028458@qq.com>
2024-03-14 16:46:24 +0800

    add

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -2,28 +2,37 @@
   <div class="app_container">
     <div class="common-layout">
       <el-container>
-        <el-header class="header">
+        <el-aside width="240px" class="aside">
           <div class="islogo">
             <img src="../assets/images/headerLogo.png" />
           </div>
-          <div class="rightbtn">
-            <el-button type="primary" class="ml-2" @click="edit"
-              >编辑</el-button
-            >
-            <el-button @click="layout">退出</el-button>
+          <el-menu :collapse="isCollapse" unique-opened router>
+            <MenuList :list="store.getters.menuData" />
+          </el-menu>
+        </el-aside>
+        <el-container class="container-r">
+          <div class="header-out">
+            <el-header class="header">
+              <div class="group-btn">
+                <el-button type="primary" class="ml-2" @click="clickCollapse"
+                  >编辑</el-button
+                >
+              </div>
+              <div class="rightbtn">
+                <el-button type="primary" class="ml-2" @click="edit"
+                  >编辑</el-button
+                >
+                <el-button @click="layout">退出</el-button>
+              </div>
+            </el-header>
           </div>
-        </el-header>
-        <el-container>
-          <el-aside width="240px" class="aside">
-            <el-menu :collapse="isCollapse" unique-opened router>
-              <MenuList :list="store.getters.menuData" />
-            </el-menu>
-          </el-aside>
+          
           <el-main class="main">
             <router-view></router-view>
           </el-main>
         </el-container>
       </el-container>
+      
     </div>
   </div>
 </template>
@@ -35,7 +44,7 @@ import {useStore} from 'vuex'
 const store = useStore()    //获取store对象
 const router = useRouter();
 
-const isCollapse = ref(false);
+let isCollapse = ref(false);
 
 onMounted(() => {
   console.log("onMounted--");
@@ -52,47 +61,65 @@ const layout = () => {
 function setData (){
   console.log('setData')
 }
+function clickCollapse(){
+  console.log('111',isCollapse.value)
+  isCollapse.value = !isCollapse.value
+}
 </script>
 
 <style scoped lang="scss">
 /* 手动配置全局样式 */
-html,
-body,
-.app_container,
-.el-container {
+html,body,.app_container,.el-container {
   padding: 0;
   margin: 0;
   height: 100vh;
 }
 /* 背景颜色 */
 .aside {
-//   border-right: 1px #ddd solid;
+  border-right:1px #eee solid;
+  .islogo {
+    height: 70px;
+    background: azure;
+    img{
+      height: 30px;
+      margin: 20px 0 0 20px;
+    }
+  }
   .el-menu {
     border-right: 0;
   }
 }
-.main {
-  background-color: #f8f8f8;
-  padding: 15px;
+.container-r{
+  display: inline-block;
+}
+.header-out{
+  width: 100%;
+  padding-top: 70px;
+  position: relative;
+  border-bottom: 1px #eee solid;
 }
 .header {
   width: 100%;
-  border-bottom: 1px #ddd solid;
-  height: 80px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  background: #fff;
+  z-index: 99;
+  height: 70px;
   display: flex;
   justify-content: space-between;
-  .islogo {
-    // width: 40px;
-    height: 40px;
-    // background: #666;
-    margin: 20px;
-    img{
-      height: 100%;
-    }
+  .group-btn{
+    padding: 19px 15px 0 0;
   }
   .rightbtn {
-    float: right;
-    padding: 24px 15px 0 0;
+    padding: 19px 15px 0 0;
   }
+}
+.main {
+  width: 100%;
+  height: calc(100vh - 70px);
+  background-color: #f8f8f8;
+  padding: 15px;
 }
 </style>
